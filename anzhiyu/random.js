46daff53f8c84f30b9cae4468623d14f68ad8431
pqcu77 <linqt9@mail2.sysu.edu.cn>
2025-02-10 22:40:06 +0800

    Site updated: 2025-02-10 22:40:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/YatCPU-Lab-Notes/","2025/02/07/hello-world/","2025/02/08/OS-learning/"];function toRandomPost(){
+var posts=["2025/02/07/YatCPU-Lab-Notes/","2025/02/07/hello-world/","2025/02/08/OS-learning/","2025/02/10/Rust-Notes/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
