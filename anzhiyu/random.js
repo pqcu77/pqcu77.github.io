6ca38b5d1b5dc2f64c01eb3e402755fe0672015f
pqcu77 <linqt9@mail2.sysu.edu.cn>
2025-02-19 12:07:48 +0800

    Site updated: 2025-02-19 12:07:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/YatCPU-Lab-Notes/","2025/02/07/hello-world/","2025/02/08/OS-learning/","2025/02/10/Rust-Notes/"];function toRandomPost(){
+var posts=["2025/02/07/YatCPU-Lab-Notes/","2025/02/07/hello-world/","2025/02/08/OS-learning/","2025/02/10/Rust-Notes/","2025/02/19/Rust-String/","2025/02/19/Rust-iter/","2025/02/19/Rust-macro/","2025/02/19/Rust-smart-pointer/","2025/02/19/Rust-包-Crate/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
